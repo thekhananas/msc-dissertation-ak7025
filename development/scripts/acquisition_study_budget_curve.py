ef4258b2aafcac495961c61035f90f99f0b33f73
Anas Khan <chilloutwithanas@gmail.com>
2026-08-13 18:53:10 +0100

    [FEAT] Add command for publishing acquisition-study development budget curve

---
+++ development/scripts/acquisition_study_budget_curve.py
@@ -0,0 +1,6 @@
+"""Publish the verified acquisition-study development budget curve."""
+
+from socratic_tutor.acquisition_study.budget_curve_cli import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
