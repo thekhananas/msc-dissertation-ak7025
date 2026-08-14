1fbf8afba91bf325d67a729ae72a38f961e4ac68
Anas Khan <chilloutwithanas@gmail.com>
2026-08-14 23:34:10 +0100

    [FEAT] Add command for publishing development acquisition ablations

---
+++ development/scripts/acquisition_study_ablations.py
@@ -0,0 +1,6 @@
+"""Publish the verified development acquisition ablations."""
+
+from socratic_tutor.acquisition_study.ablation_cli import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
